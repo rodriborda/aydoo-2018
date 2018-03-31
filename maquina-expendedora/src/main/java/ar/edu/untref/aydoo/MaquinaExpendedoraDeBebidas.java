0ab122b90da49ddb696cbd53060523ba5ffbb937
Rodrigo <rodrigo.borda.92@gmail.com>
2018-03-31 19:10:22 -0300

    Agregar funcionalidad para que pase el test deberiaHacerCafeConLecheCon10DeAzucar()

--- maquina-expendedora/src/main/java/ar/edu/untref/aydoo/MaquinaExpendedoraDeBebidas.java
+++ maquina-expendedora/src/main/java/ar/edu/untref/aydoo/MaquinaExpendedoraDeBebidas.java
@@ -1,7 +1,10 @@
 package ar.edu.untref.aydoo;
 
 public class MaquinaExpendedoraDeBebidas {
+
     public void hacerCafeConLecheConNDeAzucar(Vaso unVaso, int cantidadAzucar) {
+        unVaso.setCantidadDeAzucar(cantidadAzucar);
+        unVaso.agregarCafe();
     }
 
     public void hacerTeConLecheConNDeAzucar(Vaso unVaso, int cantidadAzucar) {
--- maquina-expendedora/src/main/java/ar/edu/untref/aydoo/Vaso.java
+++ maquina-expendedora/src/main/java/ar/edu/untref/aydoo/Vaso.java
@@ -1,16 +1,33 @@
 package ar.edu.untref.aydoo;
 
 public class Vaso {
+
+    private int cantidadDeAzucar = 0;
+    private boolean cafe = false;
+    private boolean te = false;
+
     public int getCantidadDeAzucar() {
-        return cantidadDeAzucar;
+        return this.cantidadDeAzucar;
     }
 
-    public boolean tieneCafe() {
+    public void setCantidadDeAzucar(int cantidadDeAzucar){
+        this.cantidadDeAzucar = cantidadDeAzucar;
     }
 
     public boolean tieneAzucar() {
+        return this.cantidadDeAzucar > 0;
     }
 
+    public void agregarCafe(){
+        this.cafe = true;
+    }
+
+    public boolean tieneCafe() {
+        return this.cafe;
+    }
+
+
     public boolean tieneTe() {
+        return this.te;
     }
 }
