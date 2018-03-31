e994fd3d9a5d63479a41eb2c3b8fcdae99f9e927
Rodrigo <rodrigo.borda.92@gmail.com>
2018-03-31 18:58:53 -0300

    Crear las clases y metodos necesarios para que el test compile

---
+++ maquina-expendedora/src/main/java/ar/edu/untref/aydoo/MaquinaExpendedoraDeBebidas.java
@@ -0,0 +1,9 @@
+package ar.edu.untref.aydoo;
+
+public class MaquinaExpendedoraDeBebidas {
+    public void hacerCafeConLecheConNDeAzucar(Vaso unVaso, int cantidadAzucar) {
+    }
+
+    public void hacerTeConLecheConNDeAzucar(Vaso unVaso, int cantidadAzucar) {
+    }
+}
---
+++ maquina-expendedora/src/main/java/ar/edu/untref/aydoo/Vaso.java
@@ -0,0 +1,16 @@
+package ar.edu.untref.aydoo;
+
+public class Vaso {
+    public int getCantidadDeAzucar() {
+        return cantidadDeAzucar;
+    }
+
+    public boolean tieneCafe() {
+    }
+
+    public boolean tieneAzucar() {
+    }
+
+    public boolean tieneTe() {
+    }
+}
