ecbae74248f8c72a85f5bab8650bf2477877f986
Rodrigo <rodrigo.borda.92@gmail.com>
2018-04-14 20:14:46 -0300

    Configuracion inicial a partir del ejecicio Fibonacci

---
+++ Fibo2/src/main/java/ar/edu/untref/aydoo/Fibonachi.java
@@ -0,0 +1,13 @@
+package ar.edu.untref.aydoo;
+
+public class Fibonachi {
+
+    public int calcularFibonachi(int n) {
+        if (n <= 1) {
+            return n;
+        } else {
+            return calcularFibonachi(n - 1) + calcularFibonachi(n - 2);
+        }
+    }
+
+}
---
+++ Fibo2/src/main/java/ar/edu/untref/aydoo/Program.java
@@ -0,0 +1,20 @@
+package ar.edu.untref.aydoo;
+
+public class Program {
+
+    protected Program() {
+    }
+
+    public static final void main(final String[] arg) {
+
+        Fibonachi fibo = new Fibonachi();
+
+        System.out.print("fibo<" + arg[0] + ">: ");
+
+        for (int i = 0; i < Integer.parseInt(arg[0]); i++) {
+            System.out.print(fibo.calcularFibonachi(i) + " ");
+        }
+
+        System.out.println();
+    }
+}
