e5683e7cca32b30268adb8985dfc3c833f79a978
Rodrigo <rodrigo.borda.92@gmail.com>
2018-04-15 17:21:06 -0300

    Se agrega funcionalidad para mostrar la secuencia de forma directa o inversa

--- Fibo2/src/main/java/ar/edu/untref/aydoo/Program.java
+++ Fibo2/src/main/java/ar/edu/untref/aydoo/Program.java
@@ -7,24 +7,67 @@ public class Program {
 
     public static final void main(final String[] arg) {
 
+        String opcion = arg[0];
         Fibonachi fibo = new Fibonachi();
 
-        switch (arg[0]){
-            case "-o=hd":
-                System.out.print("fibo<" + arg[1] + ">: ");
-                for (int i = 0; i < Integer.parseInt(arg[1]); i++) {
-                    System.out.print(fibo.calcularFibonachi(i) + " ");
-                }
-                break;
-            case "-o=vd":
-                System.out.print("fibo<" + arg[1] + ">: ");
-                for (int i = 0; i < Integer.parseInt(arg[1]); i++) {
-                    System.out.println();
-                    System.out.println(fibo.calcularFibonachi(i));
-                }
-                break;
+        if(arg.length == 1){
+            System.out.print("fibo<" + Integer.parseInt(arg[0]) + ">: ");
+            for (int i = 0; i < Integer.parseInt(arg[0]); i++) {
+                System.out.print(fibo.calcularFibonachi(i) + " ");
+            }
+        } else {
+
+            int numero = Integer.parseInt(arg[1]);
+
+            switch (opcion) {
+                case "-o=hd":
+                    System.out.print("fibo<" + numero + ">: ");
+                    for (int i = 0; i < numero; i++) {
+                        System.out.print(fibo.calcularFibonachi(i) + " ");
+                    }
+                    break;
+
+                case "-o=hi":
+                    int[] resultado = new int[numero];
+                    for (int i = 0; i <= numero - 1; i++) {
+                        resultado[i] = fibo.calcularFibonachi(i);
+                    }
+
+                    System.out.print("fibo<" + numero + ">: ");
+
+                    for (int i = numero - 1; i >= 0; i--) {
+                        System.out.print(resultado[i] + " ");
+                    }
+                    break;
+
+                case "-o=vd":
+                    System.out.print("fibo<" + numero + ">: ");
+                    for (int i = 0; i < numero; i++) {
+                        System.out.println();
+                        System.out.println(fibo.calcularFibonachi(i));
+                    }
+                    break;
+
+                case "-o=vi":
+                    int[] resultado1 = new int[numero];
+                    for (int i = 0; i <= numero - 1; i++) {
+                        resultado1[i] = fibo.calcularFibonachi(i);
+                    }
+
+                    System.out.print("fibo<" + numero + ">: ");
+
+                    for (int i = numero - 1; i >= 0; i--) {
+                        System.out.println();
+                        System.out.println(resultado1[i]);
+                    }
+                    break;
+                default:
+                    System.out.println("Opcion Invalida");
+                    break;
+            }
         }
-        
+
         System.out.println();
+
     }
 }
