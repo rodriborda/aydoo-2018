d49a4a007ed2d5b6bbab388260d054b596b09c16
Rodrigo <rodrigo.borda.92@gmail.com>
2018-05-07 23:00:22 -0300

    Se corrige error al calcular la suma del arreglo

--- fibo3/src/main/java/ar/edu/untref/aydoo/FibonacciSumador.java
+++ fibo3/src/main/java/ar/edu/untref/aydoo/FibonacciSumador.java
@@ -27,7 +27,7 @@ public class FibonacciSumador {
 
         int total = 0;
         for (int i = 0; i < numero; i++) {
-            total += this.fibo.calcularFibonachi(numero);
+            total += this.fibo.calcularFibonachi(i);
         }
 
         return this.resultado + " " + total;
